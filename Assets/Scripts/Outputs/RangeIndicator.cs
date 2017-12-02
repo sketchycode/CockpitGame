b31043c99e248aa92f7130f47192e608601cce5c
sketchycode <jeremy@sketchyventures.com>
2017-12-02 02:46:33 -0700

    initial range gauge indicator thing

---
+++ Assets/Scripts/Outputs/RangeIndicator.cs
@@ -0,0 +1,87 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RangeIndicator : MonoBehaviour
+{
+    public RangeSegment[] Segments;
+
+    [Range(0, 1f)]
+    public float TrueValue; // value the gauge should read
+
+    private float currentValue; // value the gauge currently reads (allows for animation to true value)
+    private GameObject background;
+    private Slider slider;
+
+    public void Start()
+    {
+        background = transform.Find("Background").gameObject;
+        slider = GetComponent<Slider>();
+        currentValue = TrueValue;
+    }
+    
+    public void Update()
+    {
+        if (Segments != null && Segments.Length > 0)
+        {
+            AdjustBackgroundSegmentsIfNeeded();
+            currentValue = Mathf.Lerp(currentValue, TrueValue, Time.deltaTime);
+            slider.value = currentValue;
+        }
+    }
+
+    private void AdjustBackgroundSegmentsIfNeeded()
+    {
+        while (background.transform.childCount < Segments.Length)
+        {
+            GameObject panel = new GameObject("BackgroundRangeSegment");
+            panel.AddComponent<CanvasRenderer>();
+            panel.AddComponent<RectTransform>();
+            panel.AddComponent<Image>();
+            panel.transform.SetParent(background.transform, false);
+        }
+
+        while (background.transform.childCount > Segments.Length)
+        {
+            var go = background.transform.GetChild(0);
+            go.parent = null;
+            GameObject.Destroy(go);
+        }
+
+        var backgroundRect = background.GetComponent<RectTransform>();
+        var segmentWidthTotal = Segments.Sum(s => s.Length);
+        float left = 0;
+        for (int i=0; i<background.transform.childCount; i++)
+        {
+            var rt = background.transform.GetChild(i).GetComponent<RectTransform>();
+            var width = i == (background.transform.childCount - 1) ?
+                backgroundRect.rect.width - left :
+                backgroundRect.rect.width * (Segments[i].Length / segmentWidthTotal);
+            rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, left, width - backgroundRect.rect.width);
+            rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, 0);
+            rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 0, 0);
+            rt.anchorMin = Vector2.zero;
+            rt.anchorMax = Vector2.one;
+            left += width;
+
+            var image = background.transform.GetChild(i).GetComponent<Image>();
+            image.color = Segments[i].Status == RangeSegmentStatus.Critical ? Color.red :
+                Segments[i].Status == RangeSegmentStatus.Warning ? Color.yellow :
+                Color.green;
+        }
+    }
+}
+
+public enum RangeSegmentStatus
+{
+    Nominal,
+    Warning,
+    Critical
+}
+
+[System.Serializable]
+public class RangeSegment
+{
+    public float Length;
+    public RangeSegmentStatus Status;
+}
